7f885dc214e78dd1d9ee4249c43e88b6c14f0cce
Paridhi-git-1303 <paridhi1303.work@gmail.com>
2026-07-03 23:26:27 +0530

    Compressed model

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pickle
 import streamlit as st
 import requests
+import joblib
+joblib.dump(model, "similarity.pkl", compress=3)
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
